adc95dcd9f42b20b5796e7a6d2f83ac138f45885
Sergey Bugaev <bugaevc@gmail.com>
2023-02-03 15:30:50 +0300

    Add assert_neq

--- include/rstd/core/macros.hpp
+++ include/rstd/core/macros.hpp
@@ -1,9 +1,19 @@
+#pragma once
+
 #include <rstd/core/panicking.hpp>
 
 #define assert_eq(l, r) do { \
     auto __l = (l); \
     auto __r = (r); \
     if (__l != __r) { \
+        rstd::core::panicking::panic(); \
+    } \
+} while (false)
+
+#define assert_neq(l, r) do { \
+    auto __l = (l); \
+    auto __r = (r); \
+    if (__l == __r) { \
         rstd::core::panicking::panic(); \
     } \
 } while (false)
