1095b7958e4b7d868d4c700f1f382f0e63347b30
Sergey Bugaev <bugaevc@gmail.com>
2023-03-14 16:55:21 +0300

    Add cxxstd::mem_fn()
    This lets you refer to member functions almost like in Rust!

--- include/rstd/core/cxxstd.hpp
+++ include/rstd/core/cxxstd.hpp
@@ -67,6 +67,26 @@ struct invoke_result<F, Args...> {
 template<typename F, typename... Args>
 using invoke_result_t = typename invoke_result<F, Args...>::type;
 
+template<typename M, typename T>
+class mem_fn_t {
+    M T::*pm;
+
+public:
+    mem_fn_t(M T::*pm)
+        : pm(pm)
+    { }
+
+    template<typename I, typename... Args>
+    invoke_result_t<M T::*, I, Args...> operator()(I &&instance, Args &&...args) {
+        return (forward<I>(instance).*pm)(forward<Args>(args)...);
+    }
+};
+
+template<typename M, typename T>
+mem_fn_t<M, T> mem_fn(M T::*pm) {
+    return mem_fn_t<M, T>(pm);
+}
+
 template<bool B, typename T = void>
 struct enable_if { };
 
